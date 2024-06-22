8e3b1e343d60e075fd66137f79fe3a35455b00ed
Curt Brown <88054943+Kato2695@users.noreply.github.com>
2024-06-22 21:42:06 +0100

    Update

---
+++ sql_capstone_project/8_Optimal_skills.sql
@@ -0,0 +1,83 @@
+/* 
+What are the most optimal skills to learn?
+- use previous queries (top demanded and top paid) to gain an understanding on the most optimal skill
+- focus on remote jobs with specified salaries
+*/
+
+-- CTE to calculate the demand count of each skill for Data Analyst positions with remote work and a specified salary
+WITH skills_demand AS (
+    SELECT
+        sd.skill_id,
+        sd.skills,
+        COUNT(sjd.job_id) AS demand_count
+    FROM job_postings_fact jpf
+    INNER JOIN skills_job_dim sjd ON jpf.job_id = sjd.job_id
+    INNER JOIN skills_dim sd ON sjd.skill_id = sd.skill_id
+    WHERE
+        jpf.job_title_short = 'Data Analyst'
+        AND jpf.salary_year_avg IS NOT NULL
+        AND jpf.job_work_from_home = True
+    GROUP BY
+        sd.skill_id,
+        sd.skills
+),
+
+-- CTE to calculate the average salary for each skill for Data Analyst positions with remote work and a specified salary
+average_salary AS (
+    SELECT
+        sd.skill_id,
+        sd.skills,
+        ROUND(AVG(jpf.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact jpf
+    INNER JOIN skills_job_dim sjd ON jpf.job_id = sjd.job_id
+    INNER JOIN skills_dim sd ON sjd.skill_id = sd.skill_id
+    WHERE
+        jpf.job_title_short = 'Data Analyst'
+        AND jpf.salary_year_avg IS NOT NULL
+        AND jpf.job_work_from_home = True
+    GROUP BY
+        sd.skill_id,
+        sd.skills
+)
+
+-- Final query to combine the demand count and average salary data
+SELECT
+    sd.skill_id,
+    sd.skills,
+    sd.demand_count,
+    avg_salary
+FROM
+    skills_demand sd
+INNER JOIN average_salary ON sd.skill_id = average_salary.skill_id
+WHERE
+    demand_count > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT
+    25;
+
+-- Rewritten query to be more concise
+
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) as demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT
+    25;
