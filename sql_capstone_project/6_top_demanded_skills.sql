07330d20e3377434e12a2578f33093bb72696039
Curt Brown <88054943+Kato2695@users.noreply.github.com>
2024-06-22 20:39:06 +0100

    Update

---
+++ sql_capstone_project/6_top_demanded_skills.sql
@@ -0,0 +1,20 @@
+/*
+What are the most in-demand skills for data analysts?
+- identify the top 5 in-demand skills for a data analyst
+- focus on all job postings
+*/
+
+SELECT
+    skills,
+    COUNT(sjd.job_id) as demand_count
+FROM job_postings_fact jpf
+INNER JOIN skills_job_dim sjd ON jpf.job_id = sjd.job_id
+INNER JOIN skills_dim sd ON sjd.skill_id = sd.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+--    AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
